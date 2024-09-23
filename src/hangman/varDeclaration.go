ca9a66bc5c653383d9712d8531db6d21fdcd3e39
Léo Velazquez <velazquez.leo@protonmail.com>
2024-09-23 12:17:28 +0200

    Push de la v1
    La première version est assez conséquente pour être le jeu final mais je ferais d'autres updates

---
+++ src/hangman/varDeclaration.go
@@ -0,0 +1,84 @@
+package hangman
+
+import (
+	"fmt"
+	"os"
+)
+
+// Détermine si le mode debug est activé
+var DebugMode bool = false
+var DebugModePtr = &DebugMode
+
+// Contient la liste des mots possibles
+var WordList []string
+var WordListPtr = &WordList
+
+// Contient la liste des ASCII arts
+var ASCIIArts map[string]string
+var ASCIIArtsPtr = &ASCIIArts
+
+// Nombre de vies restantes au joueur
+var RemainingLives int
+var RemainingLivesPtr = &RemainingLives
+
+// Mot actuel en slices de runes
+var CurrentWord []rune
+var CurrentWordPtr = &CurrentWord
+
+// Lettres déjà trouvées dans le mot
+var FoundLetters []rune
+var FoundLettersPtr = &FoundLetters
+
+// Lettres déjà essayées
+var TriedLetters []rune
+var TriedLettersPtr = &TriedLetters
+
+// Nombre d'essais
+var Tries int
+var TriesPtr = &Tries
+
+// Score actuel
+var Score int
+var ScorePtr = &Score
+
+type LeaderboardEntry struct {
+	name  string
+	score int
+}
+
+// Initialise toute les déclarations de variables
+func VarInit() {
+	wordListInit()
+	AsciiArtsInit()
+	*RemainingLivesPtr = 9
+}
+
+// Lis le fichier containant les mots et les ajoute à la liste
+func wordListInit() {
+	ReadFile("words.txt")
+	wordListFile, err := ReadFile("words.txt")
+	if err != nil {
+		panic(err)
+	}
+	*WordListPtr = SplitAndFormatLines(wordListFile)
+}
+
+func AsciiArtsInit() {
+	// Initialise ASCIIArts
+	*ASCIIArtsPtr = make(map[string]string)
+
+	// Lis la liste des fichiers du dossier ASCII_arts
+	files, err := os.ReadDir("./ASCII_arts")
+	if err != nil {
+		fmt.Println("Erreur lors de la lecture du dossier ASCII_arts")
+	}
+
+	// Associe chaque fichier à son contenu dans la map ASCIIArts
+	for _, file := range files {
+		asciiArt, err := ReadFile("./ASCII_arts/" + file.Name())
+		if err != nil {
+			fmt.Println("Erreur lors de la lecture du fichier " + file.Name())
+		}
+		(*ASCIIArtsPtr)[file.Name()] = asciiArt
+	}
+}
